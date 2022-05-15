a973196989ba1d0e1db437d0d85ff6be36dd415e
Shmoofs <timothyalsaudi@gmail.com>
2022-05-15 02:53:36 -0700

    Initizalized a few things to begin development :)

---
+++ src/main/java/net/entityFerret.java
@@ -0,0 +1,29 @@
+package net;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class entityFerret implements ModInitializer {
+ 
+    /*
+     * Registers our Cube Entity under the ID "entitytesting:cube".
+     *
+     * The entity is registered under the SpawnGroup#CREATURE category, which is what most animals and passive/neutral mobs use.
+     * It has a hitbox size of .75x.75, or 12 "pixels" wide (3/4ths of a block).
+     */
+    public static final EntityType<ferret> CUBE = Registry.register(
+            Registry.ENTITY_TYPE,
+            new Identifier("entitytesting", "cube"),
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, ferret::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build()
+    );
+ 
+    @Override
+    public void onInitialize() {
+ 
+    }
+}
